0dac163236f2b78beb0b25757c5ee678686c6b11
Phil <phil@Jules.local>
2018-03-30 11:25:15 -0400

    added new cellotap route

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Codelab Music', cellotap: "/cellotap" });
+});
+
+/* GET cellotap page. */
+router.get ('/cellotap', function(req, res, next){
+  res.render('cellotap', {title: "Cello Tap"});
 });
 
 module.exports = router;
